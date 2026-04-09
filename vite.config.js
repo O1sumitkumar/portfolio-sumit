1bed02c30c3e2945b7484dcfaa31721526e5dfa3
Sumit Jha <sumitjha365@gmail.com>
2026-04-09 09:17:33 +0530

    chore: add @vercel/remix dependency, remove unused Cloudflare worker modules, and update Vite configuration for Vercel integration

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import {
   vitePlugin as remix,
 } from '@remix-run/dev';
+import { vercelPreset } from '@vercel/remix/vite';
 import { defineConfig } from 'vite';
 import jsconfigPaths from 'vite-jsconfig-paths';
 import mdx from '@mdx-js/rollup';
@@ -9,8 +10,6 @@ import remarkMdxFrontmatter from 'remark-mdx-frontmatter';
 import rehypeImgSize from 'rehype-img-size';
 import rehypeSlug from 'rehype-slug';
 import rehypePrism from '@mapbox/rehype-prism';
-
-const isStorybook = process.argv[1]?.includes('storybook');
 
 export default defineConfig({
   assetsInclude: ['**/*.glb', '**/*.hdr', '**/*.glsl'],
@@ -27,6 +26,7 @@ export default defineConfig({
       providerImportSource: '@mdx-js/react',
     }),
     remix({
+      presets: [vercelPreset()],
       routes(defineRoutes) {
         return defineRoutes(route => {
           route('/', 'routes/home/route.js', { index: true });
